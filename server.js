00d06634a21d4aa7de8cfe368dc60bfd8202b0c8
Eric <ericyoung111@gmail.com>
2021-12-12 15:42:54 -0700

    a homepage is served

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 
-// const helpers = require('./utils/helpers')
+const helpers = require('./utils/helpers')
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -24,8 +24,7 @@ const sess = {
 
 app.use(session(sess));
 
-// const hbs = exphbs.create({ helpers });
-const hbs = exphbs.create();
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
